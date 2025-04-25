b7336655b467dc369c7511e9cdf35665d0ba8904
Arni Magnusson <thisisarni@gmail.com>
2025-04-26 02:40:07 +1100

    Adapt output_fit.R to SKJ 2022

--- TAF/output_fit.R
+++ TAF/output_fit.R
@@ -1,9 +1,8 @@
 ## Extract model fit results, write TAF output tables
 
-## Before: 00.par, 11.par, length.fit, plot-11.par.rep, test_plot_output,
-##         weight.fit (model)
-## After:  cpue.csv, length.comps.csv, likelihoods.csv, stats.csv,
-##         weight.comps.csv (output)
+## Before: fisheries.csv (data), 09.par, length.fit, plot-09.par.rep,
+##         test_plot_output (model)
+## After:  cpue.csv, length.comps.csv, likelihoods.csv, stats.csv, (output)
 
 library(TAF)
 taf.library(FLR4MFCL)
@@ -12,20 +11,17 @@ source("utilities.R")  # reading
 mkdir("output")
 
 # Read MFCL output files
-par <- reading("parameters", read.MFCLPar(finalPar("model")))
-rep <- reading("model estimates", read.MFCLRep(finalRep("model")))
+fisheries <- read.taf("data/fisheries.csv")
+par <- reading("parameters", read.MFCLPar("model/09.par"))
+rep <- reading("model estimates", read.MFCLRep("model/plot-09.par.rep"))
 like <- reading("likelihoods", read.MFCLLikelihood("model/test_plot_output"))
 lenfit <- reading("length fits", read.MFCLLenFit("model/length.fit"))
-wgtfit <- reading("weight fits", read.MFCLWgtFit("model/weight.fit"))
 
 # Model stats
 npar <- n_pars(par)
 objfun <- obj_fun(par)
 gradient <- max_grad(par)
-start <- file.mtime("model/00.par")
-hours <- file.mtime(finalPar("model")) - file.mtime("model/00.par")
-hours <- hours[[1]]
-stats <- data.frame(npar, objfun, gradient, start, hours)
+stats <- data.frame(npar, objfun, gradient)
 
 # Likelihoods
 likelihoods <- summary(like)
@@ -33,6 +29,7 @@ likelihoods <- as.data.frame(as.list(likelihoods$likelihood))
 names(likelihoods) <- summary(like)$component
 likelihoods$bhsteep <- likelihoods$effort_dev <- NULL
 likelihoods$catchability_dev <- likelihoods$total <- NULL
+likelihoods$weight_comp <- NULL
 likelihoods$penalties <- obj_fun(par) - sum(likelihoods)
 
 # CPUE
@@ -40,30 +37,24 @@ obs <- as.data.frame(cpue_obs(rep))
 pred <- as.data.frame(cpue_pred(rep))
 names(obs)[names(obs) == "data"] <- "obs"
 names(pred)[names(pred) == "data"] <- "pred"
-cpue <- cbind(obs, pred["pred"])
-cpue <- cpue[cpue$unit %in% 33:37,]
-cpue$area <- as.integer(cpue$unit) - 32
+cpue <- merge(obs, pred)
+cpue <- cpue[cpue$unit %in% 32:41,]
+names(cpue)[names(cpue) == "unit"] <- "fishery"
+cpue$area <- NULL
+cpue <- merge(cpue, fisheries[c("fishery", "area")])
+cpue <- cpue[!is.na(cpue$obs),]
 cpue$obs <- exp(cpue$obs)
 cpue$pred <- exp(cpue$pred)
-cpue$age <- cpue$unit <- cpue$iter <- NULL
+cpue <- cpue[c("year", "season", "fishery", "area", "obs", "pred")]
 
 # Length comps
 length.comps <- lenfits(lenfit)
 length.comps$season <- (1 + length.comps$month) / 3
-names(length.comps)[names(length.comps) == "sample_size"] <- "ess"
-length.comps <- length.comps[c("year", "season", "fishery", "ess",
-                               "length", "obs", "pred")]
-
-# Weight comps
-weight.comps <- wgtfits(wgtfit)
-weight.comps$season <- (1 + weight.comps$month) / 3
-names(weight.comps)[names(weight.comps) == "sample_size"] <- "ess"
-weight.comps <- weight.comps[c("year", "season", "fishery", "ess",
-                               "weight", "obs", "pred")]
+length.comps <- length.comps[c("year", "season", "fishery", "length", "obs",
+                               "pred")]
 
 # Write TAF tables
 write.taf(cpue, dir="output")
 write.taf(length.comps, dir="output")
 write.taf(likelihoods, dir="output")
 write.taf(stats, dir="output")
-write.taf(weight.comps, dir="output")
