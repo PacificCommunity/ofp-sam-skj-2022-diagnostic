ba290baca62b061e2075e029b0e55f3a9dac065a
Arni Magnusson <thisisarni@gmail.com>
2025-04-26 17:01:04 +1100

    Adapt model script to SKJ 2022

--- TAF/model.R
+++ TAF/model.R
@@ -1,7 +1,7 @@
 ## Run analysis, write model results
 
-## Before: doitall.sh, mfcl.cfg, yft.age_length, yft.frq, yft.ini,
-##         yft.tag (boot/data), mfclo64 (boot/software)
+## Before: doitall.sh, mfcl.cfg, skj.frq, skj.ini, skj.tag (boot/data),
+##         mfclo64 (boot/software)
 ## After:
 
 library(TAF)
@@ -12,12 +12,11 @@ mkdir("model")
 cp("boot/software/mfclo64", "model")
 
 # Input files
-cp("boot/data/doitall.sh",     "model")
-cp("boot/data/mfcl.cfg",       "model")
-cp("boot/data/yft.age_length", "model")
-cp("boot/data/yft.frq",        "model")
-cp("boot/data/yft.ini",        "model")
-cp("boot/data/yft.tag",        "model")
+cp("boot/data/doitall.sh", "model")
+cp("boot/data/mfcl.cfg",   "model")
+cp("boot/data/skj.frq",    "model")
+cp("boot/data/skj.ini",    "model")
+cp("boot/data/skj.tag",    "model")
 
 # Run model
 setwd("model")
